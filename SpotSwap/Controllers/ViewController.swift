105c31b23d81f7668a013ca8fcc2c31fae57fd05
Yaseen AlDallash <yaseenmeald@hotmail.com>
2018-03-15 17:19:40 -0400

    updated the spot model with date string

--- SpotSwap/Controllers/ViewController.swift
+++ SpotSwap/Controllers/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func testSpot(){
+        let user = AuthenticationService.manager.getCurrentUser()!
+        let date = Date()
+        let timeFormater = DateFormatter()
+        timeFormater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = timeFormater.string(from: date)
+        print(dateString)
+        let spot = Spot(spotUID: "adg", longitude: 151.5, latitude: 135.15, timeStamp: dateString, userUID: user.uid)
+        DataBaseService.manager.addSpot(spot: spot)
+        print("Dev:",date.description)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        testSpot()
     }
 
 
--- SpotSwap/Models/Spot.swift
+++ SpotSwap/Models/Spot.swift
@@ -11,13 +11,13 @@ class Spot: Codable {
     var spotUID: String
     let longitude: Double
     let latitude: Double
-    let timeStamp: Date
+    let timeStamp: String
     var userUID: String //this is the user who created the spot
     func toJSON() -> Any {
         let jsonData = try! JSONEncoder().encode(self)
         return try! JSONSerialization.jsonObject(with: jsonData, options: [])
     }
-    init(spotUID: String, longitude: Double, latitude: Double, timeStamp: Date,userUID: String) {
+    init(spotUID: String, longitude: Double, latitude: Double, timeStamp: String,userUID: String) {
         self.spotUID = spotUID
         self.longitude = longitude
         self.latitude = latitude
