e929f8bafaecb163477c822a7c78c57a5473609b
Caroline Cruz <cruz.caroline08@gmail.com>
2018-04-09 10:21:50 -0400

    updates

---
+++ SpotSwap/View/ResetPasswordView.swift
@@ -0,0 +1,21 @@
+//
+//  ResetPasswordView.swift
+//  SpotSwap
+//
+//  Created by C4Q on 4/5/18.
+//  Copyright © 2018 Yaseen Al Dallash. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
